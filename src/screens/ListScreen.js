68c8facccd7eb865c4dd2210ca4131d17facf484
MayraAhs <alex572000s@gmail.com>
2025-04-29 16:17:14 -0600

    List Building

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,45 @@
+import React from 'react'; //library that use for get all functionality of the differents components in general
+import { View, Text, StyleSheet, FlatList} from 'react-native';
+
+const ListScreen = () => {
+    const friends = [
+        {
+            name: "friend #1",
+            age: 20
+        },
+        {
+            name: "friend #2",
+            age: 45
+        },
+        {
+            name: "friend #3",
+            age: 32
+        },
+        {
+            name: "friend #4",
+            age: 27
+        },
+        {
+            name: "friend #5",
+            age: 53
+        },
+        {
+            name: "friend #6",
+            age: 30
+        },
+    ]
+
+    return (
+        <FlatList
+            keyExtractor={friend => friend.name}
+            data={friends}
+            renderItem={({ item }) => {
+                return <Text> {item.name} - Age: {item.age}</Text>;
+            }}
+        />
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default ListScreen; 
